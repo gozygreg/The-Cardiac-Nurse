94f3c4b521cc4ea739aab9198f5e3caaf50ea485
gozygreg <goziennabuife@yahoo.com>
2022-09-29 03:29:37 +0000

    create first view template

--- cardiacnurseblog/views.py
+++ cardiacnurseblog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import NurseBlog
 
-# Create your views here.
+
+
+class BlogPage(generic.ListView):
+    """
+    view for blog page
+    """
+    model = NurseBlog
+    # content of NurseBlog table
+    queryset = NurseBlog.objects.filter(status=1).order_by('-created_on')
+    # HTML file for view to render
+    template_name = 'nurseblog.html'
+    # Limit number of blog post that appear on front page to 6
+    paginate_by = 6
