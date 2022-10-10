7e63c3b8327ecda593e25b3e5a3dbd2f9998215f
gozygreg <goziennabuife@yahoo.com>
2022-10-10 23:57:44 +0000

    Create functionality that allow site admin to add and verify users/nurse who want to join the platform. Also update user stories on this

--- cardiacnurseprofile/admin.py
+++ cardiacnurseprofile/admin.py
@@ -1,3 +1,36 @@
+"""
+Providing acess to NurseProfile in
+the admin panel of django
+"""
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from . models import NurseProfile
 
-# Register your models here.
+
+@admin.register(NurseProfile)
+class NurseProfileAdmin(SummernoteModelAdmin):
+    """
+    To control or manage how details are dispayed
+    on django admin panel
+    """
+    # prepopulated_fields = {'slug': ('nurse_name',)}
+    list_display = ('nurse_name', 'specialty', 'status')
+    list_filter = ('specialty', 'created_on')
+    search_fields = ('nurse_name', 'specialty')
+    actions = ['verify_nurse']
+    summernote_fields = ('description')
+    fields = ('nurse_name', 'specialty', 'description', 'nurse_image',)
+
+    def verify_nurse(self, request, queryset):
+        """
+        Function to verify nurse wanting
+        to join the platform
+        """
+        queryset.update(status=1)
+
+
+# class SpecialtyAdmin(admin.ModelAdmin):
+#     fields = ('specialty', )
+
+
+# admin.site.register(Specialty, SpecialtyAdmin)
--- cardiacnurseprofile/models.py
+++ cardiacnurseprofile/models.py
@@ -9,37 +9,48 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Specialty(models.Model):
-    """
-    Nurses' area of specialty
-    or area of interest
-    """
-    specialty = models.CharField(max_length=50)
+# class Specialty(models.Model):
+#     """
+#     Nurses' area of specialty
+#     or area of interest
+#     """
+#     choice = ((1, 'IHD'), (2, 'HF'), (3, 'CCU'))
 
-    class Meta:
-        """
-        Display order
-        """
-        ordering = ('specialty',)
+#     specialty = models.CharField(
+#         max_length=50, choices=choice, null=True, blank=True)
 
-    def __str__(self):
-        """
-        Display order
-        """
-        return self.specialty
+#     class Meta:
+#         """
+#         Display order
+#         """
+#         ordering = ('specialty',)
+
+#     def __str__(self):
+#         """
+#         Display order
+#         """
+#         return self.specialty
 
 
 class NurseProfile(models.Model):
+    """
+    Database model for nurse profile
+    """
+    choice = ((1, 'Ischemic Heart Disease'), (
+        2, 'Heart Failure'), (3, 'Coronary Care'), (
+            4, 'Arrythmia'), (5, 'Congenital Heart Disease'), (
+                6, 'Other'))
+
+    specialty = models.IntegerField(choices=choice, null=True, blank=True)
     nurse_name = models.CharField(
         max_length=50, unique=True, null=False, blank=False)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, blank=False)
-    specialty = models.ForeignKey(
-        Specialty, on_delete=models.PROTECT, blank=False, null=False)
+    # specialty = models.ForeignKey(
+    #     Specialty, on_delete=models.PROTECT, blank=False, null=False)
     description = models.TextField()
     nurse_image = CloudinaryField('image', null=False, blank=False)
-    status = models.IntegerField(null=False, blank=False)
-    year_qualified = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now=True)
 
     class Meta:
